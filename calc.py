049b40e5dd1b5738f42531794badd64f11323315
Erik Bowall <erik@kemi.uu.se>
2021-02-26 21:24:46 +0100

    Updated several functions, plotting of data should now happen by just selecting structure and calculation settings

--- calc.py
+++ calc.py
@@ -1,25 +1,30 @@
 from ase.calculators.gaussian import Gaussian, GaussianOptimizer
+from ase.vibrations import Vibrations, Infrared
 
 # Function that ask for calculation settings and returns a ase.calculator object
 def calc_set_up():
 
-    functionals = set(['B3LYP','PBE'])
-    basis_sets = set(['6-31+G*', '3-21G', '6-31G'])
+    methods = set(['LSDA','B3LYP','PBE', 'MPW1PW91', 'HF', 'MP2'])
+    basis_sets = set(['STO-3G', '3-21G', '6-31G', '6-31+G*', '6-311G',
+                    'cc-pVDZ', 'cc-pVTZ', 'cc-pVQZ', 'aug-cc-pVDZ',
+                    'aug-cc-pVTZ', 'aug-cc-pVQZ'])
 
     calc_dir = input("Name folder in which to place calculations (if it doesn't exist within /calc/ it will be created there): ")
     proj_name = input("Name of calculation/project: ")
 
-    # FUNCTIONALS
-    print("As of now, caculations are made using DFT. Available functionals are:")
-    for func in functionals:
-        print(func)
 
-    func = ''
-    while not (func in functionals):
-        func = input("Select functional: ")
+    # METHODS
+    print("Available methods are:")
+    for m in methods:
+        print(m)
 
-        if not (func in functionals):
-            print("Functional " + func + " doesn't exist, try gain")
+    method = ''
+    while not (method in methods):
+        method = input("Select method: ")
+
+        if not (method in methods):
+            print("method " + method + " doesn't exist, try gain")
+
 
     # BASIS SETS
     print("Available basis sets are:")
@@ -33,9 +38,45 @@ def calc_set_up():
         if not (basis in basis_sets):
             print("Basis set " + basis + " doesn't exist, try gain")
 
+
     # Returning ase.calculator object with specified settings
-    return Gaussian(label='calc/' + calc_dir + "/" + proj_name,
-                    xc=func,
+    # TODO - maybe return several calculators depending on what we need later?
+    return Gaussian(mem='4GB',
+                    nprocshared='8',
+                    label='calc/' + calc_dir + "/" + proj_name,
+                    xc=method,
                     basis=basis,
-                    scf='tight'
+                    scf=['tight','novaracc'],
+                    integral='noxctest',
+                    freq=''
+                    ), 'calc/' + calc_dir
+
+#def calc_pot_energy(struct):
+#    return struct.get_potential_energy()
+
+def opt(struct, calc):
+    opt = GaussianOptimizer(struct, calc)
+
+    print("\nBeginning optimization...")
+    opt.run(opt=['tight','maxcycles=100'])
+    print("Optimization completed!")
+
+    return struct
+
+def vib(struct, proj_path):
+    #struct.calc = Gaussian(label='calc/' + proj_name + '/freq',
+    #                        freq='')
+    print("\nBeginning calculation of IR frequencies...")
+
+    struct.calc = Gaussian(mem='4GB',
+                    nprocshared='8',
+                    label=proj_path + '/vib',
                     )
+    #struct.get_potential_energy()
+    ir = Infrared(struct)
+    ir.clean()
+    ir.run()
+
+    print("Calculation of vibrational frequencies completed!\nPrinting spectrum...")
+
+    return struct, ir.get_spectrum()
--- main.py
+++ main.py
@@ -2,18 +2,31 @@ from ase import atoms
 from ase.calculators.gaussian import Gaussian, GaussianOptimizer
 from ase.io import *
 from ase.optimize import BFGS
-from ase.vibrations import Vibrations
 from struct_io import *
 from calc import *
+import os
+import matplotlib.pyplot as plt
 
 # Default structure path
 struct_path = '/home/erik/RUN/adv_py_course/adpy_proj/structures/'
+# Set GAUSS_EXEDIR to your Gaussian path
+os.environ["GAUSS_EXEDIR"] = "/home/sw_old/gaussian/g09.d01/intel/bsd:/home/sw_old/gaussian/g09.d01/intel/local:/home/sw_old/gaussian/g09.d01/intel/extras:/home/sw_old/gaussian/g09.d01/intel:/home/sw_old/gaussian/g09.d01/intel/linda8.2/opteron-linux/bin"
 
 # Assigning structure to be treated to 'struct' (type = ase.Atoms)
 struct = select_struct(struct_path)
-print("Succesfully loaded structure " + struct.get_chemical_formula(mode='hill'))
+print("\nSuccesfully loaded structure " + struct.get_chemical_formula(mode='hill'))
 
-# Setting up calculation. Files are placed in chosen directory within /calc/
-calc = calc_set_up()
-
+# Setting up calculation and perform. Files are placed in chosen directory within /calc/
+calc, proj_path = calc_set_up()
 struct.calc = calc
+#print(struct.get_potential_energy())
+
+# Optimization
+    # TODO - ask if user wants to optimise structure or calculate vibrations right away
+struct = opt(struct,calc)
+
+# Calculation of vibrations, summary of IR frequencies and intensities is printed
+struct, spectrum = vib(struct, proj_path)
+
+# spectrum given as a list of lists with frequencies and intensities
+plt.plot(spectrum[0], spectrum[1])
